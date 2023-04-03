f706ab8eac02aae914a3d47840a294a101f734d0
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-04-03 18:53:12 +0300

    Setting button width, color, and display set country images

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -14,9 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
+        //Setting flag border width (from the CAlayer)
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        //Setting border color (from the CA layer)
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        //button2.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0)
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+        
+        //calling the method
+        askQuestion()
+    }
+    
+    //Method that shows 3 random flag images on the screen
+    func askQuestion(){
+        //assigns UI image to button                        describes which state of the button should be changed
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
